8ff8a1cb8fa1ab8ea5d851ed99c6c3ea712c34bb
Matt Wood <m.wood0904@gmail.com>
2018-07-20 13:19:25 -0500

    added point system

--- commands.py
+++ commands.py
@@ -2,6 +2,7 @@ import pprint
 import random
 import requests
 import pdb
+import json
 
 pp = pprint.PrettyPrinter(indent=4, depth=2)
 
@@ -18,3 +19,21 @@ def subreddit(name):
     json = res.json()
     random_url = random.choice(json['data']['children'])['data']['url']
     return random_url
+
+def goodBoyPoint(name):
+    json_data = open('./score.json').read()
+    score = json.loads(json_data)
+    if name in score:
+      print('NAME FOUND')
+      score[name] += 1
+    else:
+      print('NAME NOT FOUND')
+      score[name] = 0
+
+    pp.pprint(score)
+    with open('./score.json', 'w') as output:
+        json.dump(score, output)
+
+    message = 'You gave {} one good boy point! Now they have {}.'.format(name, score[name])
+
+    return message

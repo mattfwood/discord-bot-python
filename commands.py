f15480caa66f9c51d00b5c39cbf69ea57c4c3781
Matt Wood <m.wood0904@gmail.com>
2018-07-20 12:06:51 -0500

    added procfile

--- commands.py
+++ commands.py
@@ -7,12 +7,19 @@ pp = pprint.PrettyPrinter(indent=4, depth=2)
 
 # pick a random item from a list, separated by commas
 def decide(message):
-  winner = random.choice(message.split(','))
-  return winner.strip()
+    winner = random.choice(message.split(','))
+    return winner.strip()
+
+# get a random item from the top 20 items on a subreddit
+def subreddit(name):
+    res = requests.get(
+        'https://www.reddit.com/r/{}/hot.json?limit=20'.format(name))
+    pp.pprint(res.json())
+
 
 def wholesome():
-  res = requests.get('https://www.reddit.com/r/wholesomeMemes/hot.json?limit=100')
-  pdb.set_trace()
-  pp.pprint(res.json()['data'])
-  # print(random.choice(res.json()['children']).data.url)
-  # return random.choice(res.json()['children']).data.url
+    res = requests.get(
+        'https://www.reddit.com/r/wholesomeMemes/hot.json?limit=20')
+    pp.pprint(res.json())
+    # print(random.choice(res.json()['children']).data.url)
+    # return random.choice(res.json()['children']).data.url

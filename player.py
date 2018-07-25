950456adfa881c69dd0e266849b48dc03cde141e
Matt Wood <m.wood0904@gmail.com>
2018-07-25 15:41:50 -0500

    added to response messages

---
+++ player.py
@@ -0,0 +1,8 @@
+class player:
+    def __init__(self, user)
+        self.discord_id = user['discord_id']
+        self.score = user['score']
+        self.last_updated = user['last_updated']
+
+    def add_point():
+        pass
--- point_system.py
+++ point_system.py
@@ -34,7 +34,7 @@ def find_player(discord_id):
 
 
 def add_user(discord_id):
-    new_player={
+    new_player = {
         'discord_id': discord_id,
         'points': 1,
         'last_updated': int(time() / 60)
@@ -46,28 +46,28 @@ def add_user(discord_id):
 
 def add_point(discord_id):
     if discord_id in id_list:
-        player_index=id_list.index(discord_id)
-        player=players_list[player_index]
+        player_index = id_list.index(discord_id)
+        player = players_list[player_index]
         # Check if points are on cooldown
         if point_available(player):
             player['points'] += 1
-            player['last_updated']=(time() / 60)
+            player['last_updated'] = (time() / 60)
             fb.patch('/players/{}'.format(key), player)
             return 'You gave {} one good boy point! Now they have {}.'.format(
                 player, player['points'])
         else:
-            current_minutes=(time() / 60)
-            time_diff=int(current_minutes - player['last_updated'])
-            return "It's too soon to give another good boy point to {}! ({} minutes left)".format(player, (60 - time_diff))
+            current_minutes = (time() / 60)
+            time_diff = int(current_minutes - player['last_updated'])
+            return "It's too soon to give another good boy point to {}! ({} minutes left)".format(player['discord_id'], (60 - time_diff))
     else:
         # Add user
         return add_user(discord_id)
 
 
 def point_available(player):
-    last_updated=player['last_updated']
-    current_minutes=(time() / 60)
-    time_diff=int(current_minutes - last_updated)
+    last_updated = player['last_updated']
+    current_minutes = (time() / 60)
+    time_diff = int(current_minutes - last_updated)
 
     if time_diff > 60:
         return True
@@ -77,23 +77,23 @@ def point_available(player):
 
 def flip_coin(amount, player_name):
     pp.pprint(player_name)
-    player=find_player(player_name)
-    points=player['points']
+    player = find_player(player_name)
+    points = player['points']
     print(player)
     # If the user has enough points to bet
     if player['points'] >= amount:
-        win=random.choice([True, False])
+        win = random.choice([True, False])
         if win:
             # gain amount bet
             player['points'] += amount
             fb.patch('/players/{}'.format(player_name), player)
-            return 'You won {} points! Now you have {}.'.format(amount, player['points'])
+            return 'Winner! You won {} points! Now you have {}.'.format(amount, player['points'])
 
         else:
             # lose amount
             player['points'] -= amount
             fb.patch('/players/{}'.format(player_name), player)
-            return 'You lost {} points! Now you have {}.'.format(amount, player['points'])
+            return 'You stinky loser. You lost {} points! Now you have {}.'.format(amount, player['points'])
     else:
         print('NOT ENOUGH POINTS')
         return "You can't bet {} points, you only have {}!".format(amount, player['points'])

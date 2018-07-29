9b12176946f11b0ee69d868bed4663abf06fa6dd
Matt Wood <m.wood0904@gmail.com>
2018-07-28 21:46:53 -0500

    fixed command list styling

--- show_commands.py
+++ show_commands.py
@@ -3,7 +3,7 @@ import types
 
 # print(commands.__dict__)
 def show_commands():
-  command_list = ['**__Commands:__**']
+  command_list = ['__Commands:__']
   ignore_imports = ['random', 'requests', 'pdb', 'json', 'add_point', 'flip_coin', 'find_player', 'items', 'buy_item', 'attack_enemy']
   for name, command in commands.__dict__.items():
       valid_name = name not in ignore_imports and '__' not in name
@@ -12,7 +12,7 @@ def show_commands():
 
       if not_built_in and valid_name and is_function:
           description = command.__doc__
-          command_list.append(f"**{name}**: {description}")
+          command_list.append(f"`**{name.uppercase()}**: {description}`")
 
   message = '\n'.join(command_list)
   print(message)

a3243176a02a9c5e51aa24200ea96709bd534daf
Matt Wood <m.wood0904@gmail.com>
2018-07-28 20:43:46 -0500

    fixed point machine task

--- item_tasks.py
+++ item_tasks.py
@@ -4,7 +4,7 @@ fb = firebase.FirebaseApplication(
     'https://discord-bot-db.firebaseio.com', None)
 
 
-def add_point_task(discord_id):
+def add_point_task():
     """
     Every 10 minutes, check all users who have
     point incrementing items and give them that many points
@@ -12,7 +12,8 @@ def add_point_task(discord_id):
     players = fb.get('/players', None)
     for key, player in players.items():
         if 'Point Machine' in player['items']:
-            point_amount = player['items'].count('Point Machine') * 2
+            print(player['items']['Point Machine'])
+            point_amount = player['items']['Point Machine'] * 2
             print(f"Giving {point_amount} points to {player['discord_id']}")
             new_total = player['points'] + point_amount
             update_points(player, new_total)
@@ -21,4 +22,4 @@ def add_point_task(discord_id):
 
 
 if __name__ == '__main__':
-    add_point_task('GreatBearShark')
+    add_point_task()

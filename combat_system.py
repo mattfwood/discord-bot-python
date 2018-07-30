9b7a9d105f23e868e4c32c6817ba1aa651a6706a
Matt Wood <m.wood0904@gmail.com>
2018-07-30 17:49:13 -0500

    Update combat_system.py

--- combat_system.py
+++ combat_system.py
@@ -148,6 +148,7 @@ def attack_boss(discord_id: str) -> str:
                 attack += 10
 
             add_to_attacked(discord_id, boss=True, amount=attack)
+            return f'{discord_id} attacked for {amount}'
         else:
             return "You've already attacked this boss!"
 

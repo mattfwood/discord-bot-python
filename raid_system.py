19398a8a67f898be7b4d7c320bdfb8a85d3fa90d
Matt Wood <m.wood0904@gmail.com>
2018-07-31 15:42:30 -0500

    added raid start and starting hp

--- raid_system.py
+++ raid_system.py
@@ -1,5 +1,6 @@
 from firebase import firebase
 from point_system import update_points, find_player
+from combat_system import random_encounter
 
 fb = firebase.FirebaseApplication(
     'https://discord-bot-db.firebaseio.com', None)
@@ -7,20 +8,22 @@ fb = firebase.FirebaseApplication(
 
 def raid_fund(amount, discord_id):
     raid = fb.get('/raid', None)
-    if raid:
+    if raid['active'] is True:
+        return 'A raid has already started!'
+
+    if raid and raid['active'] is False:
         # Add raid fund amount
         raid['fund'] += amount
         if 'players' in raid:
-            raid['players'].append(discord_id)
+            raid['players'].append({ discord_id: 30 })
             pass
         else:
-            raid['players'] = [discord_id]
+            raid['players'] = [{ discord_id: 30 }]
         fb.patch(f'/raid/', raid)
         if raid['fund'] < 1000:
             return f"You added {amount} points to the raid fund, bringing the total to {raid['fund']} ({1000 - raid['fund']} remaining to start)"
         else:
-            raise Exception('Raid Not Implemented')
-
+            return start_raid()
     else:
         # Create raid
         new_raid = {
@@ -34,3 +37,9 @@ def raid_fund(amount, discord_id):
 
 def start_raid():
     raid = fb.get('/raid', None)
+    # Mark raid as active
+    raid['active'] = True
+    raid['boss'] = random_encounter()
+    raid['boss']['health'] = 300
+    fb.patch(f'/raid/', raid)
+    return 'Raid has started! Take turns attacking to try and defeat the boss.'

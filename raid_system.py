10c0907cd2b997bb951593264954a9ae3f9e81c0
Matt Wood <m.wood0904@gmail.com>
2018-07-31 16:02:58 -0500

    added raid combat

--- raid_system.py
+++ raid_system.py
@@ -1,3 +1,4 @@
+from random import randint
 from firebase import firebase
 from point_system import update_points, find_player
 from combat_system import random_encounter
@@ -40,6 +41,60 @@ def start_raid():
     # Mark raid as active
     raid['active'] = True
     raid['boss'] = random_encounter()
-    raid['boss']['health'] = 300
+    raid['boss']['health'] = 100
     fb.patch(f'/raid/', raid)
     return 'Raid has started! Take turns attacking to try and defeat the boss.'
+
+def raid_attack(discord_id):
+    raid = fb.get('/raid', None)
+    player = find_player(discord_id)
+    combat_text = []
+    if raid['active'] is True:
+        # Check that player is alive
+        player_in_raid = discord_id in raid['players']
+        if player_in_raid is False:
+            return 'Sorry, you did not fund this raid and cannot participate'
+
+        # Calculate damage
+        damage = randint(1, 10)
+
+        # Apply damage to boss
+        raid['boss']['health'] -= damage
+
+        combat_text.append(f'You dealt {damage} to {raid['boss']['name']}! ({raid['boss']['health']} HP left)')
+
+        # Check if boss is dead
+        boss_dead = raid['boss']['health'] <= 0
+
+        if boss_dead:
+            return raid_win(combat_text)
+
+        # Deal damage to player
+        boss_damage = randint(1, 10)
+        raid['players'][discord_id] -= boss_damage
+
+        player_dead = raid['players'][discord_id] <= 0
+
+        # Check that player is still alive
+        if player_dead:
+            combat_text.append(f"{raid['boss']['name']} dealt **{boss_damage}** and killed you! Hopefully one of your raid members will avenge your death.")
+
+        # Check that any players are still alive
+        no_remaining_players = max(raid['players']) < 1
+
+        if no_remaining_players:
+            return raid_loss(combat_text)
+
+        return '\n'.join(combat_text)
+    else:
+        return "There isn't an active raid."
+
+def raid_win(combat_text):
+    combat_text.append("You've defeated the raid boss! Make sure Matt actually adds the points here at some point.")
+    return '\n'.join(combat_text)
+
+def raid_loss(combat_text):
+    combat_text.append('All raid members have died! The raid has ended.')
+    return '\n'.join(combat_text)
+
+def raid_reset():
